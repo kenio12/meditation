40fbf51c6013bc2559e5ee7dd3df7086af667062
umemotokei <keikeikun@icloud.com>
2022-07-25 21:31:59 +0900

    init ja + en

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:meditation/generated/l10n.dart';
 
 import 'view/screens/home_screen.dart';
 
@@ -9,6 +11,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Meditation",
+      localizationsDelegates: [
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: S.delegate.supportedLocales,
       theme: ThemeData.dark(),
       home: HomeScreen(),
     );
